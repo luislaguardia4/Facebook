06636c2e0711d155c24aaab370b6b63d55bef60a
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2023-03-06 12:07:24 +0800

    Update app.java

--- forgotpass/app.java
+++ forgotpass/app.java
@@ -1,74 +1,72 @@
 package feb23;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 
-public class ForgotPasswordPanel extends JPanel implements ActionListener {
+public class ForgotPassword implements ActionListener {
 
+    private JFrame frame;
+    private JPanel panel;
     private JLabel emailLabel;
-    private JTextField emailTextField;
-    private JButton resetPasswordButton;
+    private JTextField emailField;
+    private JButton resetButton;
 
-    public ForgotPasswordPanel() {
-        // Set the panel layout
-        setLayout(new BorderLayout());
+    public ForgotPassword() {
+        // Create the JFrame and set its properties
+        frame = new JFrame("Forgot Password");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLocationRelativeTo(null);
+
+        // Create the JPanel and set its layout
+        panel = new JPanel(new GridBagLayout());
 
         // Create the email label and text field
         emailLabel = new JLabel("Email:");
-        emailTextField = new JTextField(20);
+        emailField = new JTextField(20);
 
-        // Create the reset password button
-        resetPasswordButton = new JButton("Reset Password");
+        // Create the reset button
+        resetButton = new JButton("Reset Password");
 
-        // Set the reset password button's action listener
-        resetPasswordButton.addActionListener(this);
+        // Set the action listener for the reset button
+        resetButton.addActionListener(this);
 
-        // Create a panel to hold the email label and text field
-        JPanel emailPanel = new JPanel(new GridBagLayout());
-
-        // Add the email label and text field to the email panel
+        // Add the components to the panel using GridBagConstraints
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.insets = new Insets(5, 5, 5, 5);
         gbc.gridx = 0;
         gbc.gridy = 0;
-        emailPanel.add(emailLabel, gbc);
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(emailLabel, gbc);
 
         gbc.gridx = 1;
-        emailPanel.add(emailTextField, gbc);
+        gbc.anchor = GridBagConstraints.EAST;
+        panel.add(emailField, gbc);
 
-        // Create a panel to hold the reset password button
-        JPanel buttonPanel = new JPanel(new GridBagLayout());
-
-        // Add the reset password button to the button panel
-        gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.insets = new Insets(10, 0, 0, 0);
         gbc.gridx = 0;
-        gbc.gridy = 0;
-        buttonPanel.add(resetPasswordButton, gbc);
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(resetButton, gbc);
 
-        // Add the email panel and button panel to the main panel
-        add(emailPanel, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
+        // Add the panel to the frame and make it visible
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == resetPasswordButton) {
+        if (e.getSource() == resetButton) {
             // Get the email address entered by the user
-            String email = emailTextField.getText();
+            String email = emailField.getText();
 
             // Send the password reset email to the email address
             sendPasswordResetEmail(email);
 
             // Show a message dialog to inform the user that the password reset email has been sent
-            JOptionPane.showMessageDialog(this, "A password reset email has been sent to " + email + ".", "Password Reset", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(panel, "A password reset email has been sent to " + email + ".", "Password Reset", JOptionPane.INFORMATION_MESSAGE);
         }
     }
 
@@ -78,17 +76,12 @@ public class ForgotPasswordPanel extends JPanel implements ActionListener {
     }
 
     public static void main(String[] args) {
-        // Create a JFrame to hold the Forgot Password panel
-        JFrame frame = new JFrame("Forgot Password");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Add the Forgot Password panel to the JFrame
-        ForgotPasswordPanel panel = new ForgotPasswordPanel();
-        frame.getContentPane().add(panel);
-
-        // Display the JFrame
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        // Create an instance of the ForgotPassword class
+        ForgotPassword forgotPassword = new ForgotPassword();
     }
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
 }
